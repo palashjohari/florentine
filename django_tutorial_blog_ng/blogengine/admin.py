d99313b1eff49bc694ad9d90f1c602182fabe4bb
palashjohari <palashjohari@gmail.com>
2014-10-13 21:02:48 +0530

    added author field

--- django_tutorial_blog_ng/blogengine/admin.py
+++ django_tutorial_blog_ng/blogengine/admin.py
@@ -1,7 +1,13 @@
 import models
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-#class PostAdmin(admin.ModelAdmin):
-#    prepopulated_fields = {"slug": ("title",)}
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    exclude = ('author',)
 
-admin.site.register(models.Post)
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        obj.save()
+
+admin.site.register(models.Post, PostAdmin)
